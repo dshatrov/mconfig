b392e3fa5e8a1b884607526e3e22668c7e0b8fbb
Dmitry Shatrov <shatrov@gmail.com>
2011-06-23 03:47:06 +0400

    Initial commit.

---
+++ mconfig/config.h
@@ -0,0 +1,401 @@
+/*  MConfig - C++ library for working with configuration files
+    Copyright (C) 2011 Dmitry Shatrov
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef __MCONFIG__CONFIG__H__
+#define __MCONFIG__CONFIG__H__
+
+
+#include <libmary/libmary.h>
+
+
+namespace MConfig {
+
+using namespace M;
+
+class SectionEntry : public HashEntry<>
+{
+    friend class Section;
+    friend class Config;
+
+public:
+    enum Type
+    {
+	Type_Option,
+	Type_Section,
+	Type_Invalid
+    };
+
+private:
+    Ref<String> name_str;
+
+    Type const type;
+
+public:
+    Type getType () const
+    {
+	return type;
+    }
+
+    ConstMemory getName () const
+    {
+	return name_str->mem();
+    }
+
+    SectionEntry (Type const type,
+		  ConstMemory const &entry_name)
+	: type (type)
+    {
+	name_str = grab (new String (entry_name));
+    }
+
+    virtual ~SectionEntry () {}
+};
+
+class Value : public IntrusiveListElement<>
+{
+private:
+    Ref<String> value_str;
+
+    enum CachedType {
+	CachedType_None,
+	CachedType_Double,
+	CachedType_Int64,
+	CachedType_Uint64
+    };
+
+    CachedType cached_type;
+    bool bad_value;
+
+    union {
+	double cached_double;
+	Int64  cached_int64;
+	Uint64 cached_uint64;
+    } cached_value;
+
+public:
+    void setValue (ConstMemory const &mem)
+    {
+	value_str = grab (new String (mem));
+    }
+
+    Result getAsDouble (double *ret_val);
+
+    Result getAsInt64 (Int64 *ret_val);
+
+    Result getAsUint64 (Uint64 *ret_val);
+
+    Ref<String> getAsString ()
+    {
+	return value_str;
+    }
+
+    ConstMemory mem ()
+    {
+	return value_str->mem();
+    }
+
+    Value ()
+	: cached_type (CachedType_None),
+	  bad_value (false)
+    {
+    }
+};
+
+class Option : public SectionEntry
+{
+private:
+    Ref<String> value_str;
+
+    typedef IntrusiveList<Value> ValueList;
+
+    ValueList value_list;
+
+public:
+    void addValue (ConstMemory const &mem)
+    {
+	Value * const value = new Value;
+	value->setValue (mem);
+	value_list.append (value);
+    }
+
+    void removeValues ()
+    {
+	{
+	    ValueList::iter iter (value_list);
+	    while (!value_list.iter_done (iter)) {
+		Value * const value = value_list.iter_next (iter);
+		delete value;
+	    }
+	}
+
+	value_list.clear ();
+    }
+
+    Value* getValue ()
+    {
+	return value_list.getFirst();
+    }
+
+    void dump (OutputStream *outs,
+	       unsigned      nest_level);
+
+    Option (ConstMemory const &option_name)
+	: SectionEntry (SectionEntry::Type_Option, option_name)
+    {
+    }
+
+    ~Option ()
+    {
+	removeValues ();
+    }
+
+  // Iterators.
+
+    class iter
+    {
+	friend class Option;
+
+    private:
+	ValueList::iter iter_;
+
+    public:
+	iter ()
+	{
+	}
+
+	iter (Option &option)
+	    : iter_ (option.value_list)
+	{
+	}
+    };
+
+    void iter_begin (iter &iter)
+    {
+	value_list.iter_begin (iter.iter_);
+    }
+
+    Value* iter_next (iter &iter)
+    {
+	return value_list.iter_next (iter.iter_);
+    }
+
+    bool iter_done (iter &iter)
+    {
+	return value_list.iter_done (iter.iter_);
+    }
+};
+
+class Section : public SectionEntry
+{
+private:
+    typedef Hash< SectionEntry,
+		  Memory,
+		  MemberExtractor< SectionEntry,
+				   Ref<String>,
+				   &SectionEntry::name_str,
+				   Memory,
+				   AccessorExtractor< String,
+						      Memory,
+						      &String::mem > >,
+		  MemoryComparator<> >
+	    SectionEntryHash;
+
+    SectionEntryHash section_entry_hash;
+
+public:
+    SectionEntry* getSectionEntry (ConstMemory const &path,
+				   bool create = false,
+				   SectionEntry::Type section_entry_type = SectionEntry::Type_Invalid);
+
+    Option* getOption (ConstMemory const &path,
+		       bool create = false);
+
+    Section* getSection (ConstMemory const &path,
+			 bool create = false);
+
+    SectionEntry* getSectionEntry_nopath (ConstMemory const &section_entry_name);
+
+    Option* getOption_nopath (ConstMemory const &option_name,
+			      bool create = false);
+
+    Section* getSection_nopath (ConstMemory const &section_name,
+				bool create = false);
+
+    // Takes ownership of @option.
+    void addOption (Option * const option)
+    {
+	section_entry_hash.add (option);
+    }
+
+    // Takes ownership of @section.
+    void addSection (Section * const section)
+    {
+	section_entry_hash.add (section);
+    }
+
+    void removeSectionEntry (SectionEntry * const section_entry)
+    {
+	section_entry_hash.remove (section_entry);
+	delete section_entry;
+    }
+
+    void dump (OutputStream *outs,
+	       unsigned      nest_level = 0);
+
+    void dumpBody (OutputStream *outs,
+		   unsigned      nest_level = 0);
+
+    Section (ConstMemory const &section_name)
+	: SectionEntry (SectionEntry::Type_Section, section_name)
+    {
+    }
+
+    ~Section ();
+
+  // Iterators.
+
+    class iter
+    {
+	friend class Section;
+
+    private:
+	SectionEntryHash::iter iter_;
+
+    public:
+	iter ()
+	{
+	}
+
+	iter (Section &section)
+	    : iter_ (section.section_entry_hash)
+	{
+	}
+    };
+
+    void iter_begin (iter &iter)
+    {
+	section_entry_hash.iter_begin (iter.iter_);
+    }
+
+    SectionEntry* iter_next (iter &iter)
+    {
+	return section_entry_hash.iter_next (iter.iter_);
+    }
+
+    bool iter_done (iter &iter)
+    {
+	return section_entry_hash.iter_done (iter.iter_);
+    }
+};
+
+class GetResult
+{
+public:
+    enum Value {
+	Invalid = 0,
+	Default,
+	Success
+    };
+    operator Value () const { return value; }
+    GetResult (Value const value) : value (value) {}
+    GetResult () {}
+private:
+    Value value;
+};
+
+class Config
+{
+private:
+    Section root_section;
+
+public:
+    // Helper method to avoid excessive explicit calls to getRootSection().
+    Option* getOption (ConstMemory const &path,
+		       bool const create = false)
+    {
+	return root_section.getOption (path, create);
+    }
+
+    // Helper method to avoid excessive explicit calls to getRootSection().
+    Section* getSection (ConstMemory const &path,
+			 bool const create = false)
+    {
+	return root_section.getSection (path, create);
+    }
+
+    Option* setOption (ConstMemory const &path,
+		       ConstMemory const &value);
+
+    ConstMemory getString (ConstMemory const &path);
+
+    ConstMemory getString_default (ConstMemory const &path,
+				   ConstMemory const &default_value)
+    {
+	ConstMemory const str = getString (path);
+	if (str.len() == 0) {
+	    return default_value;
+	}
+	return str;
+    }
+
+    GetResult getUint64 (ConstMemory const &path,
+			 Uint64 *ret_value);
+
+    GetResult getUint64_default (ConstMemory const &path,
+				 Uint64 * const ret_value,
+				 Uint64   const default_value)
+    {
+	GetResult const res = getUint64 (path, ret_value);
+	if (res == GetResult::Default) {
+	    if (ret_value) {
+		*ret_value = default_value;
+	    }
+	    return GetResult::Default;
+	}
+	return res;
+    }
+
+    enum BooleanValue {
+	Boolean_Invalid,
+	Boolean_Default,
+	Boolean_True,
+	Boolean_False
+    };
+
+    BooleanValue getBoolean (ConstMemory const &path);
+
+    Section* getRootSection ()
+    {
+	return &root_section;
+    }
+
+    void dump (OutputStream *outs,
+	       unsigned      nest_level = 0);
+
+    Config ()
+	: root_section ("root")
+    {
+    }
+};
+
+}
+
+
+#endif /* __MCONFIG__CONFIG__H__ */
+
---
+++ mconfig/config_parser.cpp
@@ -0,0 +1,331 @@
+/*  MConfig - C++ library for working with configuration files
+    Copyright (C) 2011 Dmitry Shatrov
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include <libmary/types.h>
+#include <cctype>
+
+#include <mycpp/native_file.h>
+#include <mycpp/cached_file.h>
+
+#include <pargen/parser.h>
+
+#include <scruffy/preprocessor.h>
+#include <scruffy/list_pp_item_stream.h>
+#include <scruffy/pp_item_stream_token_stream.h>
+#include <scruffy/checkpoint_tracker.h>
+
+#include <mconfig/mconfig_pargen.h>
+
+#include <mconfig/config_parser.h>
+
+
+using namespace M;
+
+namespace MConfig {
+
+class ConfigParser
+{
+public:
+    MyCpp::List<Section*> sections;
+
+    Scruffy::CheckpointTracker checkpoint_tracker;
+
+    ConfigParser (Config * const config)
+    {
+	sections.append (config->getRootSection());
+    }
+};
+
+// Characters banned from words: " { } , ; # =
+//
+// " is used for string literals, which are provided by pargen with quotes
+// included.
+//
+bool
+mconfig_word_token_match_func (MyCpp::ConstMemoryDesc const &token_mem,
+			       void * const /* token_user_ptr */,
+			       void * const /* user_data */)
+{
+    logD_ (_func, ConstMemory (token_mem));
+
+    if (token_mem.getLength() == 0)
+	return false;
+
+    char const *token = (char const*) token_mem.getMemory();
+    if (token [0] == '{' ||
+	token [0] == '}' ||
+	token [0] == ',' ||
+	token [0] == ';' ||
+	token [0] == '#' ||
+	token [0] == '=')
+    {
+	return false;
+    }
+
+    if (token [0] == '"'
+	&& (token_mem.getLength() == 1
+	    || token [token_mem.getLength() - 1] != '"'))
+    {
+	return false;
+    }
+
+    return true;
+}
+
+static bool
+whitespaceBetweenWordsNeeded (ConstMemory const &left,
+			      ConstMemory const &right)
+{
+    if (left .len() == 0 ||
+	right.len() == 0)
+    {
+	return false;
+    }
+
+    if (isalnum (left.mem() [left.len() - 1]) &&
+	isalnum (right.mem() [0]))
+    {
+	return true;
+    }
+
+    return false;
+}
+
+static Ref<String>
+wordsToString (MyCpp::List<MConfig_Word*> * const mt_nonnull words)
+{
+    Size str_len = 0;
+    {
+	ConstMemory prv_word;
+	MyCpp::List<MConfig_Word*>::DataIterator iter (*words);
+	while (!iter.done ()) {
+	    MConfig_Word * const word_ = iter.next();
+
+	    ConstMemory word = word_->any_token->token;
+	    // Stripping quotes from string literals.
+	    if (word.len() && word.mem() [0] == '"') {
+		assert (word.len() >= 2);
+		word = word.region (1, word.len() - 2);
+	    }
+
+	    str_len += word.len();
+
+	    if (whitespaceBetweenWordsNeeded (prv_word, word))
+		++str_len;
+
+	    prv_word = word;
+	}
+    }
+
+    logD_ (_func, "str_len: ", str_len);
+
+    Ref<String> str = grab (new String (str_len));
+
+    {
+	Size pos = 0;
+	ConstMemory prv_word;
+	MyCpp::List<MConfig_Word*>::DataIterator iter (*words);
+	while (!iter.done ()) {
+	    MConfig_Word * const word_ = iter.next();
+
+	    ConstMemory word = word_->any_token->token;
+	    logD_ (_func, "word: ", word);
+	    // Stripping quotes from string literals.
+	    if (word.len() && word.mem() [0] == '"') {
+		assert (word.len() >= 2);
+		word = word.region (1, word.len() - 2);
+	    }
+
+	    if (whitespaceBetweenWordsNeeded (prv_word, word)) {
+		str->mem().mem() [pos] = ' ';
+		++pos;
+	    }
+
+	    memcpy (str->mem().mem() + pos, word.mem(), word.len());
+	    pos += word.len();
+
+	    prv_word = word;
+	}
+    }
+
+    logD_ (_func, "str: ", str);
+
+    return str;
+}
+
+static Ref<String>
+keyToString (MConfig_Key * const key)
+{
+    return wordsToString (&key->words);
+}
+
+bool
+mconfig_begin_section (MConfig_Section       * const section,
+		       Pargen::ParserControl * const /* parser_control */,
+		       void                  * const _self)
+{
+    ConfigParser * const self = static_cast <ConfigParser*> (_self);
+
+    Ref<String> section_name = keyToString (section->name);
+
+    logD_ (_func, "section: ", section_name);
+
+    Section *opts_section = self->sections.getLast()->getSection (section_name->mem());
+    if (!opts_section) {
+	opts_section = new Section (keyToString (section->name)->mem());
+	self->sections.getLast()->addSection (opts_section);
+    }
+
+    self->sections.append (opts_section);
+    self->checkpoint_tracker.addUnconditionalCancellable (
+	    MyCpp::grab (new Scruffy::Cancellable_ListElement<Section*> (
+		    self->sections,
+		    self->sections.last)));
+
+    return true;
+}
+
+bool
+mconfig_accept_option (MConfig_Option         * const option,
+		       Pargen::ParserControl  * const /* parser_control */,
+		       void                   * const _self)
+{
+    ConfigParser * const self = static_cast <ConfigParser*> (_self);
+
+    if (self->sections.isEmpty()) {
+	logE_ (_func, "option not within any section");
+	return false;
+    }
+
+    Section * const section = self->sections.getLast();
+
+    MConfig_Option_KeyValue *option__key_value = NULL;
+
+    MConfig_Key *key_elem = NULL;
+    switch (option->option_type) {
+	case MConfig_Option::_KeyValue: {
+	    option__key_value = static_cast <MConfig_Option_KeyValue*> (option);
+	    key_elem = option__key_value->key;
+	} break;
+	case MConfig_Option::_Key: {
+	    MConfig_Option_Key * const option__key = static_cast <MConfig_Option_Key*> (option);
+	    key_elem = option__key->key;
+	} break;
+	default:
+	    unreachable ();
+    }
+
+    Ref<String> key = keyToString (key_elem);
+
+    logD_ (_func, "option: ", key);
+
+    Option *opts_option = section->getOption (key->mem());
+    if (opts_option) {
+	opts_option->removeValues ();
+    } else {
+	opts_option = new Option (key->mem());
+	self->sections.getLast()->addOption (opts_option);
+    }
+
+    if (option__key_value) {
+	MConfig_Value *value = option__key_value->value;
+	for (;;) {
+	    switch (value->value_type) {
+		case MConfig_Value::_List: {
+		    MConfig_Value_List * const value__list = static_cast <MConfig_Value_List*> (value);
+		    logD_ (_func, "value: ", wordsToString (&value__list->words));
+		    opts_option->addValue (wordsToString (&value__list->words)->mem());
+		    value = value__list->value;
+		    logD_ (_func, "new value: 0x", fmt_hex, (UintPtr) value);
+		} break;
+		case MConfig_Value::_Word: {
+		    MConfig_Value_Word * const value__word = static_cast <MConfig_Value_Word*> (value);
+		    logD_ (_func, "value: ", wordsToString (&value__word->words));
+		    opts_option->addValue (wordsToString (&value__word->words)->mem());
+		    value = NULL;
+		} break;
+		default:
+		    unreachable ();
+	    }
+
+	    if (!value)
+		break;
+	}
+    }
+
+    logD_ (_func, "done");
+
+    return true;
+}
+
+Result parseConfig (ConstMemory const &filename,
+		    Config * const config)
+try {
+    MyCpp::Ref<Pargen::Grammar> grammar = create_mconfig_grammar ();
+
+    MyCpp::Ref<MyCpp::File> file = MyCpp::File::createDefault (filename,
+							       0 /* open_flags */,
+							       MyCpp::AccessMode::ReadOnly);
+    file = MyCpp::grab (new MyCpp::CachedFile (file, (1 << 14) /* page_size */, 64 /* max_pages */));
+
+    MyCpp::Ref<Scruffy::CppPreprocessor> const preprocessor = MyCpp::grab (new Scruffy::CppPreprocessor (file));
+    preprocessor->performPreprocessing ();
+
+    MyCpp::Ref< MyCpp::List_< MyCpp::Ref<Scruffy::PpItem>, MyCpp::SimplyReferenced > > pp_items =
+	    preprocessor->getPpItems ();
+
+    MyCpp::Ref<Scruffy::PpItemStream> pp_stream =
+	    MyCpp::grab (static_cast <Scruffy::PpItemStream*> (
+		    new Scruffy::ListPpItemStream (pp_items->first, MyLang::FilePosition ())));
+
+    MyCpp::Ref<Scruffy::PpItemStreamTokenStream> token_stream =
+	    MyCpp::grab (new Scruffy::PpItemStreamTokenStream (pp_stream));
+    token_stream->setNewlineReplacement (";");
+
+    ConfigParser config_parser (config);
+
+    Pargen::ParserElement *mconfig_elem = NULL;
+    Pargen::parse (token_stream,
+		   &config_parser.checkpoint_tracker,
+		   &config_parser,
+		   grammar,
+		   &mconfig_elem,
+		   MyCpp::ConstMemoryDesc::forString ("default"),
+		   // TODO Set 'upwards_jumps' to true for non-AST mode.
+		   Pargen::createParserConfig (false /* upwards_jumps */),
+		   false /* debug_dump */);
+
+    if (mconfig_elem == NULL ||
+	token_stream->getNextToken ().getLength () > 0)
+    {
+	logE_ (_func, "EOF not reached");
+	return Result::Failure;
+    }
+
+    dump_mconfig_grammar (static_cast <MConfig_Grammar*> (mconfig_elem));
+    MyCpp::errf->pflush ();
+
+    return Result::Success;
+} catch (...) {
+    logE_ (_func, "exception");
+    return Result::Failure;
+}
+
+}
+
